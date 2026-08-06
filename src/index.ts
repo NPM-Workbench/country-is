23b023a61e604d0e1345524d85a8ce7498cd5264
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-08-06 15:12:10 +0530

    [feat] Get Multi Caller Loc Info
    - added new functionality that helps get multi location info
    - response based on mandatory array of ips and fields props
    - errors would be handled and returned by api service provider

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./types/index.js";
 export * from "./info/index.js";
 export * from "./get-caller-loc-info/index.js";
 export * from "./get-loc-info-by-ip/index.js";
+export * from "./get-multi-caller-loc-info/index.js";
